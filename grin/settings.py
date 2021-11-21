8afd1df191c593f4e6a05ddcb487a86a219a2ff2
rogitun <rogitun@gmail.com>
2021-11-22 01:38:04 +0900

    8

--- grin/settings.py
+++ grin/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('SECRET_KEY')
+SECRET_KEY = 'django-insecure-(^*z2&*v+@0dn$sh!p6n68xeqi9@m*#11k)-*&3uiol2v1j+4g'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DEBUG')
 
